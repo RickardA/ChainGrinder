d7cb06b8c86b487db8c1afa687f8de1b7a90ef57
Rickard Andersson <rickard.andersson98@gmail.com>
2020-07-01 18:42:56 +0200

    Program sturcture

---
+++ classes/Program.js
@@ -0,0 +1,11 @@
+const Grinder = require('./classes/Grinder')
+const ChainGuard = require('./classes/ChainGuard')
+const Relay = require('./classes/Relay')
+const Gpio = require('pigpio').Gpio
+
+const grinder = new Grinder()
+const chainGuard = new ChainGuard()
+
+const pushChainInput = new Gpio(process.env.CHAINGUARD_PUSH_INPUT_PIN,{mode: Gpio.INPUT, alert: true})
+const grinderInput = new Gpio(process.env.GRINDER_LOWERED_INPUT,{mode: Gpio.INPUT, alert: true})
+//const magic = new Relay(19,false)
