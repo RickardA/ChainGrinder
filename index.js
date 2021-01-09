3ab3546974a34a9c45c4d6e6e29237e438ccd611
RickardA <rickard.andersson98@gmail.com>
2021-01-09 11:45:00 +0100

    added power off button

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
-require('dotenv').config({ path: '/home/pi/ChainGrinder/.env' })
+require('dotenv').config( { path: '/home/pi/ChainGrinder/.env' } )
 const express = require('express')
 const app = express()
 const Program = require('./classes/Program')
 const { getState, setSettings, setToothsLeft, setTotalNumberOfTooths, setStatus, setLengthGrindingActive } = require('./globals')
 const Socket = require('./socket')
 const MyEmitter = require('./classes/MyEvent')
+const exec = require('child_process').exec;
 
 const myEmitter = new MyEmitter()
 
@@ -48,10 +49,10 @@ async function handleCommand(msg) {
             break;
         case 'NUMBEROFTOOTHS':
 			setTotalNumberOfTooths(msg.value)
-        break;
+            break;
         case 'LENGTHGRINDINGACTIVE':
             setLengthGrindingActive(msg.value)
-        break;
+            break;
         case 'START':
             program.startProgram()
             break;
@@ -79,6 +80,10 @@ async function handleCommand(msg) {
             break;
         case 'QUITCHECKLENGTHGRINDING':
             program.quitCheckLengthGrinding()
+            break;
+        case 'SHUTDOWN':
+            program.exit()
+            exec('shutdown now', function(error, stdout, stderr){ callback(stdout); });
             break;
     }
 }
